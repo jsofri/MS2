0c171930c8823895c3794130c6bb03f75babd2b6
yehonatansofri <58885453+yehonatansofri@users.noreply.github.com>
2020-01-16 16:19:26 +0200

    minimal documentation

--- MatrixAdapter.h
+++ MatrixAdapter.h
@@ -13,7 +13,11 @@
 
 #define REGEX "[-]{0,1}[\\d]+[.]{0,1}[\\d]{0,}"
 
-
+//Matrix Adapter can make matrix object from string
+//each time you enter a string that represents a row
+//string contains int seperated by commas
+//e.g. "1, -2, 3, 4, 5"
+//when you finished adding all rows, call getMatrix()
 class MatrixAdapter {
  public:
   void addRow(string);
