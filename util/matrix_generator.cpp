9989fa43145ff605a08306d2aed247124e2db216
ronyut <ronyut@gmail.com>
2020-01-21 10:27:23 +0200

    Update matrix_generator.cpp

--- util/matrix_generator.cpp
+++ util/matrix_generator.cpp
@@ -2,45 +2,55 @@
 // Created by yehonatan on 19/01/2020.
 //
 
+#define MAX_LINE_LENGTH 2048
+
+#include <builders/matrix_builder.h>
 #include "matrix_generator.h"
 
-  //method get a string and file name and appends the string in specified file
-void MatrixGenerator::printAndClose(string str, string file_name) {
+Matrix<int> MatrixGenerator::randomMatrix(int rank) {
+    int x, y, i, j;
+    Matrix<int> matrix;
 
-  FILE *fp;
+    for (i = 0; i < rank; i++) {
+        vector<int> row;
+        for (j = 0; j < rank; j++) {
+            x = (rand() % (RAND + 1)); // 0 to RAND
+            y = (rand() % 101) - 1; // 0 to 100
 
-  fp = fopen(file_name.c_str(), "a");
+            if (!((i == rank -1 && j == rank - 1) || (i == 0 && j == 0)) && (x + y) % RAND_WALL == 0) {
+                x = -1;
+            }
 
-  if (fp != NULL) {
-    fputs(str.c_str(), fp);
-  }
-
-  fclose(fp);
-}
-
-void MatrixGenerator::makeMatrices() {
-  string str, file_name;
-  int s, x, i, j, rank;
-
-  for (s = RANK, rank = RANK; rank <= 50; rank++, s++) {
-    file_name = "matrix" + to_string(s);
-
-    for (i = 0; i < s; i++) {
-      str = "";
-      for (j = 0; j < s; j++) {
-        x = (rand() % RAND) - 1;
-        str += to_string(x);
-
-        if (j != s - 1) {
-          str += ",";
+            row.push_back(x);
         }
-      }
-      str += '\n';
-      printAndClose(str, file_name);
+        matrix.addRow(row);
     }
 
-    printAndClose("0,0\n", file_name);
-    str = to_string(s - 1) + ", " + to_string(s - 1) + '\n';
-    printAndClose(str, file_name);
-  }
+    return matrix;
+}
+
+Matrix<int> MatrixGenerator::fromFile(string file_name) {
+    FILE *fp;
+
+    fp = fopen(file_name.c_str(), "r");
+
+    if (fp == NULL) {
+        perror("can't write matrix to file");
+        exit(1);
+    }
+
+    char line[MAX_LINE_LENGTH];
+    auto mb = new MatrixBuilder();
+
+    // add each row to matrix
+    while (fgets(line, MAX_LINE_LENGTH, fp)) {
+        string str = Stringer::getAllCharsInCharArray(line);
+        mb -> addRow(str);
+    }
+
+    // close file
+    fclose(fp);
+
+    auto matrix = mb -> getMatrix();
+    return matrix;
 }
