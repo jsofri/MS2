925cc9d1a79a27ce1e1adf5f3b570aaab5266091
ronyut <ronyut@gmail.com>
2020-01-21 10:20:25 +0200

    Update matrix_astar.h

--- searchers/matrix_astar.h
+++ searchers/matrix_astar.h
@@ -5,16 +5,28 @@
 #ifndef MS2_MATRIX_ASTAR_H
 #define MS2_MATRIX_ASTAR_H
 
-#include "../objects/point.h"
+#include <objects/point.h>
 #include "searcher.h"
 #include <string>
 #include <queue>
+#include <vector>
 #include <unordered_map>
-#include "../objects/amdc.h"
-#include "../objects/matrix.h"
-#include "../objects/PriorityQ.h"
+#include <objects/point.h>
+#include <objects/amdc.h>
+#include <objects/matrix.h>
+#include <objects/priority_q.h>
+
+#define TYPE AMDC*
 
 using namespace std;
+
+struct ComparePriority {
+    bool operator()(TYPE p1, TYPE p2)
+    {
+        // return "true" if "p1" is ordered before "p2"
+        return p1 -> weightH > p2 -> weightH;
+    }
+};
 
 class MatrixAStar: public Searcher<Point, string> {
 public:
@@ -23,12 +35,14 @@ public:
 private:
     void runAStar();
     void pushNeighborsOf(Point);
-    static unsigned int distance(Point, Point);
     void initPathMatrix();
+    void init();
+    static unsigned int distance(Point, Point);
     Matrix<AMDC> path_matrix_;
-    PriorityQ<double, AMDC> queue_;
+    PriorityQ<TYPE> open_;
     Searchable<Point>* searchable_;
-    unordered_map<string, bool> map_;
+    unordered_map<string, PriorityQ<TYPE>::iterator> open_map_;
+    unordered_map<string, bool> close_;
 };
 
 
